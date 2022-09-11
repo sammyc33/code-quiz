0d5329dbf158b422404b097090ab8e0f63d0cf65
Sammy <sammy.campbell33@gmail.com>
2022-09-11 14:04:12 -0700

    add root

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+//Define variables
+var score = document.querySelector(".score");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+
+//Var array with questions and answers 
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+];
+
+//Start game starts the game when the start button is clicked
+
+//Timer
+
+//Questions
+
+//Question answer function (if, else, etc)
+
+//Keep score
+
+//End game, display game over!
+
+//Add initials and score
