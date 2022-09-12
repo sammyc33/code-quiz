41f4108606c51d841e44891742b3654a56892531
Sammy <sammy.campbell33@gmail.com>
2022-09-11 20:41:21 -0700

    add timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,106 @@
-//Var array with questions and answers 
+//Var array with questions, options, and answers 
 var questions = [
     {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        question: "Question 1: Commonly used data types DO NOT include:",
+        options: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+        answer: "c. alerts"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        question: "Question 2: The condition in an if / else statement is enclosed within ____.",
+        options: ["a. quotes", "b. curly brackets", "c. parentheses", "d. square brackets"],
+        answer: "c. parentheses"
     },
+    {
+        question: "Question 3 : How do you create a function in JavaScript",
+        options: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
+        answer: "b"
+    },
+    {
+        question: "Question 4 : How do you call a function named myFunction?",
+        options: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
+        answer: "c"
+    }
+    {
+        question: "Questions 5 : String values must be enclosed within _____ when being assigned to variables.",
+        options: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+        answer: "c"
+    }
 ];
 
 //Define remaining variables
-var correctAns = 0;
-var questionNum = 0;
-var scoreResult;
-var questionIndex = 0;
-var score = document.querySelector(".score");
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
+var welcome = document.querySelector("#introduction");
+var startBtn = document.querySelector("#start_button");
+var introPage =document.querySelector("#intro_page");
 
+var questionPage = document.querySelector("#question_page");
+var askQuestion = document.querySelector("#ask_question");
+
+var reactButtons = document.querySelectorAll(".choices");
+var answerBtn1 = document.querySelector("#answer_btn1");
+var answerBtn2 = document.querySelector("#answer_btn2");
+var answerBtn3 = document.querySelector("#answer_btn3");
+var answerBtn4 = document.querySelector("#answer_btn4");
+
+var checkLine = document.querySelector("#check_line");
+var scoreBoard = document.querySelector("#submit_page");
+var finalScore = document.querySelector("#final_score");
+var userInitial =document.querySelector("#initial");
+
+var submitBtn =document.querySelector("#submit_btn");
+var highScorePage =document.querySelector("#highscore_page");
+var scoreRecord =document.querySelector("#score_record");
+var scoreCheck =document.querySelector("#score_check");
+var finish =document.querySelector("#finish");
+
+var backBtn =document.querySelector("#back_btn");
+var clearBtn=document.querySelector("#clear_btn");
 
 
 // Clicking start button starts the timer and game begins
 
+function newQuiz() {
+    questionIndex = 0;
+    totalTime = 60;
+    timeLeft.textContent = totalTime;
+    initialInput.textContent = "";
 
-//Timer
+    startDiv.style.display = "none";
+    questionDiv.style.display = "block";
+    timer.style.display = "block";
+    timesUp.style.display = "none";
+
+    var startTimer = setInterval(function() {
+        totalTime--;
+        timeLeft.textContent = totalTime;
+        if(totalTime <= 0) {
+            clearInterval(startTimer);
+            if (questionIndex < questions.length - 1) {
+                gameOver();
+            }
+        }
+    },1000);
+
+    showQuiz();
+};
+
+function startQuiz () {
+    introPage.style.display = "none";
+    questionPage.style.display = "block";
+    questionNumber = 0
+    countdown();
+    showQuestion(questionNumber);
+  
+}
 
 //Questions
 
-//Question answer function (if, else, etc)
 
 //Keep score
 
 //End game, display game over!
 
 //Add initials and score
+
+// Go back
+
+// Clear Scores
