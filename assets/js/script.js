92776a453b1f73da2bdf16b1280391a0c2723c84
Sammy <sammy.campbell33@gmail.com>
2022-09-12 21:47:32 -0700

    update styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,72 +28,12 @@ var questions = [
 ];
 
 //Define remaining variables
-var welcome = document.querySelector("#introduction");
-var startBtn = document.querySelector("#start_button");
-var introPage =document.querySelector("#intro_page");
-
-var questionPage = document.querySelector("#question_page");
-var askQuestion = document.querySelector("#ask_question");
-
-var reactButtons = document.querySelectorAll(".choices");
-var answerBtn1 = document.querySelector("#answer_btn1");
-var answerBtn2 = document.querySelector("#answer_btn2");
-var answerBtn3 = document.querySelector("#answer_btn3");
-var answerBtn4 = document.querySelector("#answer_btn4");
-
-var checkLine = document.querySelector("#check_line");
-var scoreBoard = document.querySelector("#submit_page");
-var finalScore = document.querySelector("#final_score");
-var userInitial =document.querySelector("#initial");
-
-var submitBtn =document.querySelector("#submit_btn");
-var highScorePage =document.querySelector("#highscore_page");
-var scoreRecord =document.querySelector("#score_record");
-var scoreCheck =document.querySelector("#score_check");
-var finish =document.querySelector("#finish");
-
-var backBtn =document.querySelector("#back_btn");
-var clearBtn=document.querySelector("#clear_btn");
 
 
 // Clicking start button starts the timer and game begins
 
-function newQuiz() {
-    questionIndex = 0;
-    totalTime = 60;
-    timeLeft.textContent = totalTime;
-    initialInput.textContent = "";
-
-    startDiv.style.display = "none";
-    questionDiv.style.display = "block";
-    timer.style.display = "block";
-    timesUp.style.display = "none";
-
-    var startTimer = setInterval(function() {
-        totalTime--;
-        timeLeft.textContent = totalTime;
-        if(totalTime <= 0) {
-            clearInterval(startTimer);
-            if (questionIndex < questions.length - 1) {
-                gameOver();
-            }
-        }
-    },1000);
-
-    showQuiz();
-};
-
-function startQuiz () {
-    introPage.style.display = "none";
-    questionPage.style.display = "block";
-    questionNumber = 0
-    countdown();
-    showQuestion(questionNumber);
-  
-}
 
 //Questions
-
 
 //Keep score
 
