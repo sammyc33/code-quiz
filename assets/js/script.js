7dc2dbc4493c0ccdfeab93020f5cbbf9d2777d4d
Sammy <sammy.campbell33@gmail.com>
2022-09-12 22:33:20 -0700

    add timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,67 @@
 //Var array with questions, options, and answers 
 var questions = [
     {
-        question: "Question 1: Commonly used data types DO NOT include:",
+        question: "Commonly used data types DO NOT include:",
         options: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
         answer: "c. alerts"
     },
     {
-        question: "Question 2: The condition in an if / else statement is enclosed within ____.",
+        question: "The condition in an if / else statement is enclosed within ____.",
         options: ["a. quotes", "b. curly brackets", "c. parentheses", "d. square brackets"],
         answer: "c. parentheses"
     },
     {
-        question: "Question 3 : How do you create a function in JavaScript",
+        question: "How do you create a function in JavaScript",
         options: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
         answer: "b"
     },
     {
-        question: "Question 4 : How do you call a function named myFunction?",
+        question: "How do you call a function named myFunction?",
         options: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
         answer: "c"
     }
     {
-        question: "Questions 5 : String values must be enclosed within _____ when being assigned to variables.",
+        question: "String values must be enclosed within _____ when being assigned to variables.",
         options: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
         answer: "c"
     }
 ];
 
-//Define remaining variables
+//Define remaining variables and elements
+var timer;
+var 
 
+var startBtn = document.querySelector("#start-button");
+var timerEL = document.querySelector("#timer");
 
 // Clicking start button starts the timer and game begins
+function startQuiz() {
+    timerCount = 60;
+    startTimer();
+}
 
+// Start the timer
+function startTimer() {
+    timer = setInterval(function() {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) 
+}, 1000);
+}
+//Questions, once the timer has started the first question and its clickable answers will appear
+function askQuestions() {
 
-//Questions
+}
 
 //Keep score
 
 //End game, display game over!
 
-//Add initials and score
+//Add initials and score, click submit to save both (init function?)
 
 // Go back
 
 // Clear Scores
+
+// Event listener to call the startQuiz function
+startBtn.addEventListener("click", startQuiz);
